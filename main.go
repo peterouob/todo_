64b3bc4cff9c90af83ac36c11d2bcc62922a0015
林庭楷 <hellolinpeter@gmail.com>
2024-10-23 20:27:20 +0800

    cors

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func main() {
 	r := gin.Default()
+	r.Use(Cors)
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"msg": "GET METHOD",
@@ -16,4 +21,19 @@ func main() {
 		})
 	})
 	r.Run(":8084")
+}
+
+func Cors(c *gin.Context) {
+	method := c.Request.Method
+	c.Header("Access-Control-Allow-Origin", c.GetHeader("Origin"))
+	fmt.Println(c.GetHeader("Origin"))
+	c.Header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS")
+	c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Token")
+	c.Header("Access-Control-Expose-Headers", "Access-Control-Allow-Headers, Token")
+	c.Header("Access-Control-Allow-Credentials", "true")
+	if method == "OPTIONS" {
+		c.AbortWithStatus(http.StatusNoContent)
+		return
+	}
+	c.Next()
 }
