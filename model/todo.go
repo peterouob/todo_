d79bad714757b594c93b6c66bc0e2195d3a55db7
林庭楷 <hellolinpeter@gmail.com>
2024-10-26 21:55:28 +0800

    feat : todo service

--- model/todo.go
+++ model/todo.go
@@ -1,10 +1,45 @@
 package model
 
-import "time"
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"reflect"
+	"time"
+)
 
 type Todo struct {
+	Id       primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+	Title    string             `json:"title" bson:"title"`
+	Content  string             `json:"content" bson:"content"`
+	DeadTime time.Time          `json:"dead_time" bson:"deadTime"`
+	Done     bool               `default:"false" json:"done" bson:"done"`
+}
+
+type FindTodoRequest struct {
+	Done bool `default:"false" json:"done" bson:"done,omitempty"`
+}
+
+type UpdateTodoRequest struct {
 	Title    string    `json:"title" bson:"title"`
-	Content  string    `json:"content" bson:"content"`
-	DeadTime time.Time `json:"dead_time" bson:"deadTime"`
-	Done     bool      `json:"done" bson:"done"`
+	Content  string    `json:"content" bson:"content,omitempty"`
+	DeadTime time.Time `json:"dead_time" bson:"deadTime,omitempty"`
+}
+
+func NewTodo() Todo {
+	todo := Todo{}
+	todo.Id = primitive.NewObjectID()
+	setStructDefaultTag(&todo)
+	return todo
+}
+
+func setStructDefaultTag(s interface{}) {
+	val := reflect.ValueOf(s).Elem()
+
+	for i := 0; i < val.NumField(); i++ {
+		field := val.Type().Field(i)
+		if defaultTag, ok := field.Tag.Lookup("default"); ok && defaultTag == "false" {
+			if val.Field(i).Kind() == reflect.Bool && !val.Field(i).Bool() {
+				val.Field(i).SetBool(false)
+			}
+		}
+	}
 }
--- service/todo_service.go
+++ service/todo_service.go
@@ -1,23 +1,178 @@
 package service
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"errors"
+	"github.com/gin-gonic/gin"
+	"github.com/peterouob/todo_/model"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"io"
+	"net/http"
+)
 
 func GetAllTodo(c *gin.Context) {
+	data, err := findAllTodo(context.TODO())
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"code": -1,
+			"msg":  err.Error(),
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"code": 0,
+		"msg":  data,
+	})
+}
 
+func GetTodoFilterDone(c *gin.Context) {
+	var req model.FindTodoRequest
+	if err := c.BindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"code": -1,
+			"msg":  err.Error(),
+		})
+		return
+	}
+
+	data, err := findTodoFilterDone(req.Done, context.TODO())
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"code": -1,
+			"msg":  err.Error(),
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"code": 0,
+		"msg":  data,
+	})
 }
 
 func GetTodoByID(c *gin.Context) {
+	id := c.Param("id")
+	objectId, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"code": -1,
+			"msg":  err.Error(),
+		})
+		return
+	}
+	data, err := findById(objectId, context.TODO())
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"code": -1,
+			"msg":  err.Error(),
+		})
+		return
+	}
 
+	c.JSON(http.StatusOK, gin.H{
+		"code": 0,
+		"msg":  data,
+	})
 }
 
 func DeleteTodo(c *gin.Context) {
-
+	id := c.Param("id")
+	objectId, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"code": -1,
+			"msg":  err.Error(),
+		})
+		return
+	}
+	if err := deleteTodo(objectId, context.TODO()); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"code": -1,
+			"msg":  err.Error(),
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"code":      0,
+		"delete id": id,
+	})
 }
 
 func UpdateTodo(c *gin.Context) {
+	id := c.Param("id")
+	objectId, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"code": -1,
+			"msg":  err.Error(),
+		})
+		return
+	}
+	req := model.UpdateTodoRequest{}
 
+	if err := c.ShouldBind(&req); !errors.Is(err, io.EOF) && err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"code": -1,
+			"msg":  err.Error(),
+		})
+		return
+	}
+
+	if err := updateTodo(objectId, req, context.TODO()); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"code": -1,
+			"msg":  err.Error(),
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"code": 0,
+	})
 }
 
 func CreateTodo(c *gin.Context) {
+	todo := model.NewTodo()
+	var err error
+	if err = c.BindJSON(&todo); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"code": -1,
+			"msg":  err.Error(),
+		})
+		return
+	}
+	if err = createTodo(todo, context.TODO()); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"code": -1,
+			"msg":  err.Error(),
+		})
+		return
+	}
 
+	c.JSON(http.StatusOK, gin.H{
+		"code": 0,
+		"msg":  todo,
+	})
+}
+
+func Done(c *gin.Context) {
+	id := c.Param("id")
+	objectId, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"id":   id,
+			"code": -1,
+			"msg":  err.Error(),
+		})
+		return
+	}
+	if err := doneTodo(objectId, context.TODO()); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"code": -1,
+			"msg":  err.Error(),
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"code": 0,
+		"id":   id,
+	})
 }
