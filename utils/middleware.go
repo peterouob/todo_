5ff7d801f2a5243edb0751529360aa706022bd8b
林庭楷 <hellolinpeter@gmail.com>
2024-10-26 21:53:51 +0800

    feat : middleware use in valid user login

---
+++ utils/middleware.go
@@ -0,0 +1,59 @@
+package utils
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strings"
+)
+
+func Cors(c *gin.Context) {
+	method := c.Request.Method
+	c.Header("Access-Control-Allow-Origin", c.GetHeader("Origin"))
+	fmt.Println(c.GetHeader("Origin"))
+	c.Header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS")
+	c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Token")
+	c.Header("Access-Control-Expose-Headers", "Access-Control-Allow-Headers, Token")
+	c.Header("Access-Control-Allow-Credentials", "true")
+	if method == "OPTIONS" {
+		c.AbortWithStatus(http.StatusNoContent)
+		return
+	}
+	c.Next()
+}
+
+func AuthByJWT() func(c *gin.Context) {
+	return func(c *gin.Context) {
+		authHeader := c.Request.Header.Get("Authorization")
+		if authHeader == "" {
+			c.JSON(http.StatusForbidden, gin.H{
+				"code": "-1",
+				"msg:": "not have auth header",
+			})
+			c.Abort()
+			return
+		}
+		parts := strings.SplitN(authHeader, " ", 2)
+		if !(len(parts) == 2 && parts[0] == "Bearer") {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"code": -1,
+				"msg":  "Format of Authorization is wrong",
+			})
+			c.Abort()
+			return
+		}
+		_, err := VerifyToken(c, parts[1])
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"code":  -1,
+				"msg":   "Verify of Authorization is wrong",
+				"error": err.Error(),
+				"value": authHeader,
+			})
+			c.Abort()
+			return
+		}
+		c.Next()
+	}
+
+}
