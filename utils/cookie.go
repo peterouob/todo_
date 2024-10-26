127f3f63a08aa6bd24a6e13a32c94608e433cba9
林庭楷 <hellolinpeter@gmail.com>
2024-10-26 21:54:12 +0800

    feat : cookie to get uid

---
+++ utils/cookie.go
@@ -0,0 +1,14 @@
+package utils
+
+import (
+	"github.com/gin-gonic/gin"
+	"os"
+)
+
+func SetCookie(c *gin.Context, name, value string) {
+	c.SetCookie(name, value, 365*3600, "/", os.Getenv("SERVER"), false, true)
+}
+
+func RemoveCookie(c *gin.Context, key string) {
+	c.SetCookie(key, "", -1, "", os.Getenv("SERVER"), false, true)
+}
