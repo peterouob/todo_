8d2ac1e9c3e111b65d63bb34b86fc3e5dc6d641d
林庭楷 <hellolinpeter@gmail.com>
2024-10-26 21:56:33 +0800

    main

--- main.go
+++ main.go
@@ -1,47 +1,24 @@
 package main
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
 	_ "github.com/joho/godotenv/autoload"
 	"github.com/peterouob/todo_/db"
 	"github.com/peterouob/todo_/router"
 	"log"
-	"net/http"
+	"os"
 )
-
-//
-//func init() {
-//	if err := godotenv.Load(); err != nil {
-//		log.Panicf("error to load env file ... :%s", err.Error())
-//	}
-//}
 
 func main() {
 	go func() {
 		db.InitMysql()
 		db.InitMongo()
+		db.InitRedis()
 	}()
 
 	r := gin.Default()
-	r.Use(Cors)
 	router.InitRouter(r)
-	if err := r.Run(":8080"); err != nil {
+	if err := r.Run(os.Getenv("PORT")); err != nil {
 		log.Panicf("errors:%s", err.Error())
 	}
-}
-
-func Cors(c *gin.Context) {
-	method := c.Request.Method
-	c.Header("Access-Control-Allow-Origin", c.GetHeader("Origin"))
-	fmt.Println(c.GetHeader("Origin"))
-	c.Header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS")
-	c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Token")
-	c.Header("Access-Control-Expose-Headers", "Access-Control-Allow-Headers, Token")
-	c.Header("Access-Control-Allow-Credentials", "true")
-	if method == "OPTIONS" {
-		c.AbortWithStatus(http.StatusNoContent)
-		return
-	}
-	c.Next()
 }
