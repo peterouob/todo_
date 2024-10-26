89a87923669d62eda4b896d71d8532bc8f863fff
林庭楷 <hellolinpeter@gmail.com>
2024-10-26 21:55:46 +0800

    feat : router for todo and middleware

--- router/router.go
+++ router/router.go
@@ -3,10 +3,23 @@ package router
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/peterouob/todo_/service"
+	"github.com/peterouob/todo_/utils"
 )
 
 func InitRouter(r *gin.Engine) {
+	r.Use(utils.Cors)
 	r.GET("/")
 	r.POST("/login", service.LoginUser)
 	r.POST("/register", service.RegisterUser)
+	todo := r.Group("/todo")
+	todo.Use(utils.AuthByJWT())
+	{
+		todo.GET("/", service.GetAllTodo)
+		todo.GET("/:id", service.GetTodoByID)
+		todo.POST("/filter", service.GetTodoFilterDone)
+		todo.POST("/create", service.CreateTodo)
+		todo.PUT("/:id", service.UpdateTodo)
+		todo.DELETE("/:id", service.DeleteTodo)
+		todo.GET("/done/:id", service.Done)
+	}
 }
